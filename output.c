60df08c3d8ce25262985920b08a424aa75153d37
Guy Arye <arye.guy@gmail.com>
2013-02-09 20:14:25 +0200

    added missing files

---
+++ output.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "output.h"
+#include "consts.h"
+
+extern int code_index;
+extern int data_index;
+
+void print_base4(FILE *fp, int number)
+{
+   char base_digits[4] = {'0', '1', '2', '3'};
+
+   int converted_number[64];
+   int index=0;
+
+   /* convert to the indicated base */
+   while (number != 0)
+   {
+	 converted_number[index] = number % 4;
+	 number = number / 4;
+	 ++index;
+   }
+
+   /* now print the result in reverse order */
+   --index;  /* back up to last entry in the array */
+   for(  ; index>=0; index--) /* go backward through array */
+   {
+	 fprintf(fp, "%c", base_digits[converted_number[index]]);
+   }
+}
+
+void output(const char *source_filename)
+{
+	char obj_filename[MAX_FILENAME_LENGTH];
+	FILE *obj_fp;
+
+	strncpy(obj_filename, source_filename, MAX_FILENAME_LENGTH);
+	strncat(obj_filename, ".obj", MAX_FILENAME_LENGTH);
+
+	obj_fp = fopen(obj_filename, "wt");
+	if (NULL == obj_fp) {
+		perror("couldn't open obj file"); 
+		return;
+	}
+
+	print_base4(obj_fp, code_index);
+	fprintf(obj_fp, "\t");
+	print_base4(obj_fp, data_index);
+	fprintf(obj_fp, "\n");
+}
---
+++ output.h
@@ -0,0 +1,6 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+void output(const char *source_filename);
+
+#endif /* end of include guard: OUTPUT_H */
